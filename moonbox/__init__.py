8724e60a42f8da23c81e5d16bc6ecfd8e26b5ef1
Scott Olesen <ulp7@cdc.gov>
2024-03-11 23:23:43 -0400

    First draft of viz (#1)
    - Parse phases
    - Refactor & generalize functions
    - Plot by illumination

--- moonbox/__init__.py
+++ moonbox/__init__.py
@@ -1,52 +1,81 @@
 import requests
 import json
+import datetime
+import polars as pl
+import pymongo
+import multiprocessing
+
+"""Lat/long for Washington, DC"""
+coords_dc = "38.889444,-77.035278"
 
 
-def get_oneday():
-    url = "https://aa.usno.navy.mil/api/rstt/oneday"
-    params = {"date": "2024-03-05", "coords": "38.889444,-77.035278", "tz": -5}
+def now() -> datetime.datetime:
+    """Current time in UTC"""
+    return datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=0)))
+
+
+def ymd(x: datetime.datetime) -> str:
+    """Format datetime as YYYY-MM-DD"""
+    return x.strftime("%Y-%m-%d")
+
+
+def hm(x: datetime.datetime) -> str:
+    """Format datetime as HH:MM"""
+    return x.strftime("%H:%M")
+
+
+def get_usno(url, params):
     request = requests.get(url, params=params)
+    request.raise_for_status()
 
-    if not request.status_code == 200:
-        raise RuntimeError()
-
-    return request.content
+    return json.loads(request.content)
 
 
-def parse_oneday(content):
-    data = json.loads(content)
+def get_oneday(date=ymd(now()), coords=coords_dc, tz=-5):
+    url = "https://aa.usno.navy.mil/api/rstt/oneday"
+    params = {"date": date, "coords": coords, "tz": tz}
+    return get_usno(url, params)
 
+
+def parse_oneday(data):
     # confirm that the date is what we would expect
     # content['properties']['data']['year'], 'month', 'day'
-
     phenomena = data["properties"]["data"]["moondata"]
+    assert type(phenomena) == list
+    assert type(phenomena[0]) == dict
     times = {x["phen"]: x["time"] for x in phenomena}
 
     # also closest phase, current phase
+    phase = data["properties"]["data"]["curphase"]
 
-    return times
+    fracillum = data["properties"]["data"]["fracillum"]
+    assert fracillum[-1] == "%"
+    illumination = int(fracillum[0:-1])
+
+    return times | {"phase": phase, "illumination": illumination}
 
 
-def get_celnav():
+def get_celnav(
+    date=ymd(now()),
+    time=hm(now()),
+    coords=coords_dc,
+):
     url = "https://aa.usno.navy.mil/api/celnav"
-    params = {"date": "2024-03-05", "time": "08:30", "coords": "38.89,-77.03"}
-    request = requests.get(url, params=params)
-
-    if not request.status_code == 200:
-        raise RuntimeError()
-
-    return request.content
+    params = {"date": date, "time": time, "coords": coords}
+    return get_usno(url, params)
 
 
-def parse_celnav(content):
-    data = json.loads(content)
-
+def parse_celnav(data):
     # check the day, month, etc.
     # data['properties']['day']
 
-    moon = [
-        x for x in data["properties"]["data"] if "object" in x and x["object"] == "Moon"
-    ][0]
+    objects = data["properties"]["data"]
+    object_names = [x["object"] for x in objects]
+
+    if "Moon" not in object_names:
+        return None
+
+    moon = [x for x in objects if x["object"] == "Moon"][0]
 
     # results are in UTC
     assert data["properties"]["tz"] == 0
@@ -57,3 +86,73 @@ def parse_celnav(content):
         "illumination": data["properties"]["moon_illum"],
         "phase": data["properties"]["moon_phase"],
     }
+
+
+def get_phases(year=now().year):
+    url = "https://aa.usno.navy.mil/api/moon/phases/year"
+    params = {"year": str(year)}
+    return get_usno(url, params)
+
+
+def parse_phases(data):
+    phases = data["phasedata"]
+    assert len(phases) == data["numphases"]
+    # check year
+    # assert phases[0]['year'] ==
+    return [parse_phase(x) for x in phases]
+
+
+def parse_phase(x):
+    time_parts = x["time"].split(":")
+    assert len(time_parts) == 2
+    hour, minute = time_parts
+    return {
+        "phase": x["phase"],
+        "date": datetime.datetime(
+            x["year"], x["month"], x["day"], int(hour), int(minute)
+        ),
+    }
+
+
+def cache_oneday_year(year=now().year):
+    """
+    Cache one year's worth of oneday data
+    """
+    client = pymongo.MongoClient()
+    db = client.moonbox_db
+    collection = db["oneday"]
+
+    dates = (
+        pl.select(pl.date_range(pl.date(year, 1, 1), pl.date(year, 12, 31)))
+        .to_series()
+        .to_list()
+    )
+
+    for date in dates:
+        if collection.find_one({"date": date.isoformat()}) is None:
+            print(date)
+            result = get_oneday(date.isoformat())
+            record = {"date": date.isoformat(), "result": result}
+            collection.insert_one(record)
+
+
+def get_oneday_year(year=now().year):
+    """
+    Get one year's wroth of oneday data, from the cache
+    """
+    client = pymongo.MongoClient()
+    db = client.moonbox_db
+    collection = db["oneday"]
+
+    dates = (
+        pl.select(pl.date_range(pl.date(year, 1, 1), pl.date(year, 12, 31)))
+        .to_series()
+        .to_list()
+    )
+
+    results = [collection.find_one({"date": date.isoformat()}) for date in dates]
+    data = [
+        {"date": date} | parse_oneday(x["result"]) for date, x in zip(dates, results)
+    ]
+
+    return data
--- tests/test_moonbox.py
+++ tests/test_moonbox.py
@@ -1,31 +1,101 @@
+import pytest
 import moonbox
+import datetime
 import json
 
 
+@pytest.mark.slow
+def test_get_oneday():
+    data = moonbox.get_oneday()
+    moonbox.parse_oneday(data)
+
+
 def test_parse_oneday():
-    content = """
-        {"apiversion": "4.0.1", "geometry": {"coordinates": [-77.035278, 38.889444],
-        "type": "Point"}, "properties": {"data": {"closestphase": {"day": 3,
-        "month": 3, "phase": "Last Quarter", "time": "10:23", "year": 2024},
-        "curphase": "Waning Crescent", "day": 5, "day_of_week": "Tuesday",
-        "fracillum": "29%", "isdst": false, "label": null, "month": 3, "moondata":
-        [{"phen": "Rise", "time": "03:27"}, {"phen": "Upper Transit", "time": "07:53"},
-        {"phen": "Set", "time": "12:21"}], "sundata": [{"phen": "Begin Civil Twilight",
-        "time": "06:07"}, {"phen": "Rise", "time": "06:34"}, {"phen": "Upper Transit",
-        "time": "12:19"}, {"phen": "Set", "time": "18:06"}, {"phen":
-        "End Civil Twilight", "time": "18:32"}], "tz": -5.0, "year": 2024}}, "type": "Feature"}"""
+    content = {
+        "apiversion": "4.0.1",
+        "geometry": {"coordinates": [-77.035278, 38.889444], "type": "Point"},
+        "properties": {
+            "data": {
+                "closestphase": {
+                    "day": 3,
+                    "month": 3,
+                    "phase": "Last Quarter",
+                    "time": "10:23",
+                    "year": 2024,
+                },
+                "curphase": "Waning Crescent",
+                "day": 5,
+                "day_of_week": "Tuesday",
+                "fracillum": "29%",
+                "isdst": False,
+                "label": None,
+                "month": 3,
+                "moondata": [
+                    {"phen": "Rise", "time": "03:27"},
+                    {"phen": "Upper Transit", "time": "07:53"},
+                    {"phen": "Set", "time": "12:21"},
+                ],
+                "sundata": [
+                    {"phen": "Begin Civil Twilight", "time": "06:07"},
+                    {"phen": "Rise", "time": "06:34"},
+                    {"phen": "Upper Transit", "time": "12:19"},
+                    {"phen": "Set", "time": "18:06"},
+                    {"phen": "End Civil Twilight", "time": "18:32"},
+                ],
+                "tz": -5.0,
+                "year": 2024,
+            }
+        },
+        "type": "Feature",
+    }
     data = moonbox.parse_oneday(content)
-    assert data == {"Rise": "03:27", "Upper Transit": "07:53", "Set": "12:21"}
+    assert data == {
+        "Rise": "03:27",
+        "Upper Transit": "07:53",
+        "Set": "12:21",
+        "illumination": 29,
+        "phase": "Waning Crescent",
+    }
+
+
+@pytest.mark.slow
+def test_get_celnav():
+    data = moonbox.get_celnav()
+    moonbox.parse_celnav(data)
+
+
+@pytest.mark.slow()
+def test_get_celnav_no_moon():
+    """When the moon is not up, no celnav data"""
+    data = moonbox.get_celnav(date="2024-03-07", time="3:40")
+    assert moonbox.parse_celnav(data) is None
 
 
 def test_parse_celnav():
     with open("tests/data/celnav.json") as f:
-        content = f.read()
+        data = json.load(f)
 
-    data = moonbox.parse_celnav(content)
+    data = moonbox.parse_celnav(data)
     assert data == {
         "altitude": 0.570058,
         "azimuth": 128.290209,
         "illumination": 32,
         "phase": "Waning Crescent",
+    }
+
+
+@pytest.mark.slow
+def test_get_phases():
+    data = moonbox.get_phases()
+    moonbox.parse_phases(data)
+
+
+def test_parse_phases():
+    with open("tests/data/phases.json") as f:
+        data = json.load(f)
+
+    data = moonbox.parse_phases(data)
+    assert data[3] == {
+        "date": datetime.datetime(2024, 1, 25, 17, 54),
+        "phase": "Full Moon",
     }
